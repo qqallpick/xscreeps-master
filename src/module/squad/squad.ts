d9e8e1ac609aa1c16b120a9990a5a1a80939dfe8
zsuuu <729666627@qq.com>
2022-07-22 22:25:30 +0800

    四人远程小队改进

--- src/module/squad/squad.ts
+++ src/module/squad/squad.ts
@@ -127,7 +127,7 @@ export function squadFrameWork(squardID:string):void{
                         return
                     }
                 }
-                if (!squadNear(squadData,attack_flag.pos)  ){
+                if (!squadNear(squadData,attack_flag.pos)){
                     squadMove(squadData,attack_flag.pos,3)  // 如果不在攻击范围内，就移动到攻击范围内  设定为1是近战，3是远程
                 }
             } 
