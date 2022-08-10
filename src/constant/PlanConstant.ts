5f2522e60cc61ef3ca578aff21ed5e782d2f6957
zsuuu <729666627@qq.com>
2022-08-10 21:56:43 +0800

    优化布局

--- src/constant/PlanConstant.ts
+++ src/constant/PlanConstant.ts
@@ -7,12 +7,12 @@ export const icon = {
     "constructedWall": "▓",
     "rampart": "⊙",
     "storage": "▤",
-    "tower": "🔫",
-    "observer": "👀",
+    "tower": "♖",
+    "observer": "✧",
     "powerSpawn": "❂",
     "extractor": "⇌",
     "terminal": "✡",
-    "lab": "☢",
+    "lab": "☣",
     "container": "□",
     "nuker": "▲",
     "factory": "☭"
@@ -43,47 +43,46 @@ export const devPlanConstant:BluePrint = [
     /* 2级规划 */
     {x:-1,y:3,structureType:'extension',level:2},       // extension
     {x:-2,y:3,structureType:'extension',level:2},
-    {x:-2,y:2,structureType:'extension',level:2},
+    {x:-3,y:3,structureType:'extension',level:2},
     {x:-2,y:4,structureType:'extension',level:2},
     {x:-3,y:4,structureType:'extension',level:2},
-    {x:-1,y:2,structureType:'road',level:2},            // road
-    {x:1,y:2,structureType:'road',level:2},
-    {x:0,y:2,structureType:'road',level:2},
-    {x:-1,y:1,structureType:'road',level:2},
-    {x:-2,y:1,structureType:'road',level:2},
-    {x:-3,y:0,structureType:'road',level:2},
-    {x:-2,y:-1,structureType:'road',level:2},
-    {x:-1,y:-2,structureType:'road',level:2},
-    {x:-1,y:-1,structureType:'road',level:2},
-    {x:0,y:-3,structureType:'road',level:2},
-    {x:1,y:-2,structureType:'road',level:2},
-    {x:1,y:-1,structureType:'road',level:2},
-    {x:2,y:-1,structureType:'road',level:2},
-    {x:3,y:0,structureType:'road',level:2},
-    {x:2,y:1,structureType:'road',level:2},
+    {x:-1,y:2,structureType:'road',level:4},            // road
+    {x:1,y:2,structureType:'road',level:4},
+    {x:-1,y:1,structureType:'road',level:4},
+    {x:-2,y:1,structureType:'road',level:4},
+    {x:-3,y:0,structureType:'road',level:4},
+    {x:-2,y:-1,structureType:'road',level:4},
+    {x:-1,y:-2,structureType:'road',level:4},
+    {x:-1,y:-1,structureType:'road',level:4},
+    {x:0,y:-3,structureType:'road',level:4},
+    {x:1,y:-2,structureType:'road',level:4},
+    {x:1,y:-1,structureType:'road',level:4},
+    {x:2,y:-1,structureType:'road',level:4},
+    {x:3,y:0,structureType:'road',level:4},
+    {x:2,y:1,structureType:'road',level:4},
     /* 3级规划 */
     {x:-4,y:3,structureType:'extension',level:3},     // extension
     {x:-4,y:2,structureType:'extension',level:3},
-    {x:3,y:1,structureType:'extension',level:3},
+    {x:-3,y:1,structureType:'extension',level:3},
     {x:-3,y:2,structureType:'extension',level:3},
     {x:-3,y:-1,structureType:'extension',level:3},
-    {x:0,y:1,structureType:'tower',level:3},           // tower
-    {x:0,y:3,structureType:'road',level:3},             // road
-    {x:1,y:1,structureType:'road',level:3},
-    {x:-1,y:4,structureType:'road',level:3},
-    {x:-2,y:5,structureType:'road',level:3},
-    {x:-3,y:5,structureType:'road',level:3},
-    {x:-4,y:4,structureType:'road',level:3},
-    {x:-5,y:3,structureType:'road',level:3},
-    {x:-5,y:2,structureType:'road',level:3},
-    {x:-4,y:1,structureType:'road',level:3},
+    {x:-2,y:2,structureType:'tower',level:3},           // tower
+    {x:0,y:3,structureType:'road',level:4},             // road
+    {x:1,y:1,structureType:'road',level:4},
+    {x:-1,y:4,structureType:'road',level:4},
+    {x:-2,y:5,structureType:'road',level:4},
+    {x:-3,y:5,structureType:'road',level:4},
+    {x:-4,y:4,structureType:'road',level:4},
+    {x:-5,y:3,structureType:'road',level:4},
+    {x:-5,y:2,structureType:'road',level:4},
+    {x:-4,y:1,structureType:'road',level:4},
     /* 4级规划 */
-    {x:-2,y:-4,structureType:'extension',level:4},    // extension
-    {x:-2,y:-2,structureType:'extension',level:4},
-    {x:-1,y:0,structureType:'extension',level:4},
+    {x:-2,y:-3,structureType:'extension',level:4},    // extension
+    {x:-2,y:-4,structureType:'extension',level:4},
+    {x:-3,y:-2,structureType:'extension',level:4},
     {x:-3,y:-3,structureType:'extension',level:4},
     {x:-3,y:-4,structureType:'extension',level:4},
-    {x:-2,y:0,structureType:'extension',level:4},
+    {x:-4,y:0,structureType:'extension',level:4},
     {x:-4,y:-2,structureType:'extension',level:4},
     {x:-4,y:-3,structureType:'extension',level:4},
     {x:-5,y:0,structureType:'extension',level:4},
@@ -98,17 +97,17 @@ export const devPlanConstant:BluePrint = [
     {x:-1,y:-4,structureType:'road',level:4},
     /* 5级规划 */
     {x:-5,y:1,structureType:'extension',level:5},    // extension
-    {x:1,y:3,structureType:'extension',level:5},
-    {x:2,y:-2,structureType:'extension',level:5},
+    {x:1,y:-3,structureType:'extension',level:5},
+    {x:2,y:-3,structureType:'extension',level:5},
     {x:2,y:-4,structureType:'extension',level:5},
     {x:3,y:-3,structureType:'extension',level:5},
     {x:3,y:-4,structureType:'extension',level:5},
-    {x:0,y:-1,structureType:'extension',level:5},
+    {x:3,y:-2,structureType:'extension',level:5},
     {x:3,y:-1,structureType:'extension',level:5},
     {x:4,y:-2,structureType:'extension',level:5},
     {x:4,y:-3,structureType:'extension',level:5},
     {x:1,y:0,structureType:'link',level:5},         // link && tower
-    {x:-3,y:-2,structureType:'tower',level:5},
+    {x:-2,y:-2,structureType:'tower',level:5},
     {x:1,y:-4,structureType:'road',level:5},            // road
     {x:2,y:-5,structureType:'road',level:5},
     {x:3,y:-5,structureType:'road',level:5},
@@ -123,10 +122,7 @@ export const devPlanConstant:BluePrint = [
     {x:4,y:1,structureType:'road',level:6},
     {x:5,y:1,structureType:'road',level:6},
     {x:6,y:0,structureType:'road',level:6},
-    {x:1,y:4,structureType:'road',level:6},
     {x:6,y:-1,structureType:'road',level:6},
-    {x:1,y:4,structureType:'road',level:6},
-    {x:1,y:4,structureType:'road',level:6},
     {x:3,y:3,structureType:'road',level:6},
     {x:4,y:4,structureType:'road',level:6},
     {x:5,y:5,structureType:'road',level:6},
@@ -141,11 +137,11 @@ export const devPlanConstant:BluePrint = [
     {x:-1,y:-3,structureType:'extension',level:6},      // extension
     {x:-1,y:-5,structureType:'extension',level:6},
     {x:1,y:-5,structureType:'extension',level:6},
-    {x:0,y:-2,structureType:'extension',level:6},
-    {x:2,y:0,structureType:'extension',level:6},
+    {x:0,y:-4,structureType:'extension',level:6},
+    {x:4,y:0,structureType:'extension',level:6},
     {x:5,y:0,structureType:'extension',level:6},
     {x:5,y:-1,structureType:'extension',level:6},
-    {x:-5,y:5,structureType:'extension',level:6},
+    {x:0,y:-5,structureType:'extension',level:6},
     {x:-5,y:4,structureType:'extension',level:6},
     {x:-4,y:5,structureType:'extension',level:6},
     {x:2,y:4,structureType:'lab',level:6},          // lab
@@ -155,8 +151,8 @@ export const devPlanConstant:BluePrint = [
     {x:4,y:2,structureType:'lab',level:7},              // lab
     {x:4,y:3,structureType:'lab',level:7},
     {x:5,y:3,structureType:'lab',level:7},  
-    {x:2,y:-3,structureType:'tower',level:7},               // tower link
-    {x:0,y:-4,structureType:'spawn',level:7},               // spawn
+    {x:2,y:-2,structureType:'tower',level:7},               // tower link
+    {x:0,y:-2,structureType:'spawn',level:7},               // spawn
     {x:-3,y:6,structureType:'extension',level:7},    // extension
     {x:-2,y:6,structureType:'extension',level:7},
     {x:0,y:4,structureType:'extension',level:7},
@@ -169,42 +165,31 @@ export const devPlanConstant:BluePrint = [
     {x:-5,y:-4,structureType:'extension',level:7},
     {x:-1,y:6,structureType:'road',level:7},    // road
     {x:0,y:6,structureType:'road',level:7},
-    {x:-4,y:6,structureType:'road',level:7},
-    {x:-5,y:6,structureType:'road',level:7},
-    {x:-6,y:5,structureType:'road',level:7},
-    {x:-6,y:4,structureType:'road',level:7},
     {x:-6,y:1,structureType:'road',level:7},
     {x:-6,y:0,structureType:'road',level:7},
     {x:-6,y:-1,structureType:'road',level:7},
-    {x:-6,y:-4,structureType:'road',level:7},
-    {x:-6,y:-5,structureType:'road',level:7},
-    {x:6,y:-5,structureType:'road',level:7},
-    {x:1,y:-3,structureType:'factory',level:7},    // factory
+    {x:1,y:3,structureType:'factory',level:7},    // factory
     /* 8级规划 */
-    {x:4,y:-6,structureType:'road',level:8},        // road
-    {x:5,y:-6,structureType:'road',level:8},
-    {x:0,y:-6,structureType:'road',level:8},
+    {x:0,y:-6,structureType:'road',level:8},         // road
     {x:1,y:-6,structureType:'road',level:8},
     {x:-1,y:-6,structureType:'road',level:8},
-    {x:-4,y:-6,structureType:'road',level:8},
-    {x:-5,y:-6,structureType:'road',level:8},
     {x:5,y:-4,structureType:'extension',level:8},    // extension
-    {x:5,y:-5,structureType:'extension',level:8},
+    {x:-3,y:-6,structureType:'extension',level:8},
     {x:4,y:-5,structureType:'extension',level:8},
-    {x:-5,y:-5,structureType:'extension',level:8},
+    {x:-2,y:-6,structureType:'extension',level:8},
     {x:-6,y:-2,structureType:'extension',level:8},
     {x:-6,y:-3,structureType:'extension',level:8},
     {x:6,y:-2,structureType:'extension',level:8},
     {x:6,y:-3,structureType:'extension',level:8},
     {x:2,y:-6,structureType:'extension',level:8},
     {x:3,y:-6,structureType:'extension',level:8},
-    {x:-3,y:1,structureType:STRUCTURE_NUKER,level:8},    // nuker
+    {x:3,y:1,structureType:STRUCTURE_NUKER,level:8},    // nuker
     {x:6,y:1,structureType:STRUCTURE_OBSERVER,level:8},     // observer
-    {x:-4,y:0,structureType:STRUCTURE_SPAWN,level:8},       // spawn
-    {x:4,y:0,structureType:STRUCTURE_POWER_SPAWN,level:8},
-    {x:-3,y:3,structureType:STRUCTURE_TOWER,level:8},        // tower
-    {x:3,y:-2,structureType:STRUCTURE_TOWER,level:8},
-    {x:-2,y:-3,structureType:STRUCTURE_TOWER,level:8},
+    {x:-2,y:0,structureType:STRUCTURE_SPAWN,level:8},       // spawn
+    {x:2,y:0,structureType:STRUCTURE_POWER_SPAWN,level:8},
+    {x:0,y:1,structureType:STRUCTURE_TOWER,level:8},        // tower
+    {x:0,y:-1,structureType:STRUCTURE_TOWER,level:8},
+    {x:-1,y:0,structureType:STRUCTURE_TOWER,level:8},
     {x:2,y:5,structureType:'lab',level:8},      // lab
     {x:4,y:5,structureType:'lab',level:8},
     {x:5,y:2,structureType:'lab',level:8},
